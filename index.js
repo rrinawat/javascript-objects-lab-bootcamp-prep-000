ab7742e1394844bba8bd4408aa83ca6546ae97d2
Ryan Inawat <rrinawat@gmail.com>
2017-11-28 21:00:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var recipes = {}
-
+//update object non destructively
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]: value})
+}
+//update object destructively, can sitll use Object.assign, but do not declare a new target obj
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  return Object.assign(object, {key: value})
 }
